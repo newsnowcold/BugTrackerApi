ddb30a1f1335e35aaa1a9d2dfcaa1b00c80bafa3
Darrel Abello <darrel.abello@ilucent.com>
2017-09-06 00:09:51 -0700

    Initial Code drop

---
+++ BugTrackerApi/Controllers/BaseController.cs
@@ -0,0 +1,38 @@
+﻿using DBLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BugTrackerApi.Controllers
+{
+    public class BaseController : ApiController
+    {
+        private BugTrackerEntities db = new BugTrackerEntities();
+
+        public BaseController()
+        {
+        }
+
+        public BugTrackerEntities DB
+        {
+            get { return db; }
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && db != null)
+            {
+                db.Dispose();
+                db = null;
+            }
+
+            base.Dispose(disposing);
+        }
+
+
+    }
+}
---
+++ BugTrackerApi/Controllers/ValueController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BugTrackerApi.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("Value")]
+    public class ValueController : ApiController
+    {
+
+        [HttpGet]
+        [Route("Test")]
+        public IHttpActionResult Test()
+        {
+            return Ok("Ok");
+        }
+    }
+}
