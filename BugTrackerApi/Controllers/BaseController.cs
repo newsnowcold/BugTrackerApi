cf6b5e47ebd496390d5091d5864753b74c68214e
Darrel Abello <darrel.abello@ilucent.com>
2017-09-06 23:08:56 -0700

    BaseController.cs: Add more properties for easy access

--- BugTrackerApi/Controllers/BaseController.cs
+++ BugTrackerApi/Controllers/BaseController.cs
@@ -1,16 +1,25 @@
 ﻿using DBLayer;
+using ErrorCatcher.Interface;
+using ErrorCatcher.Methods;
+using ErrorCatcher.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using Microsoft.AspNet.Identity;
+using BugTrackerApi.Models;
+using BugTrackerApi.Services;
 
 namespace BugTrackerApi.Controllers
 {
     public class BaseController : ApiController
     {
-        private BugTrackerEntities db = new BugTrackerEntities();
+        public ISendCustomError customResponseMsgService = new SendCustomResponse();
+        private BugTrackerEntities db;
+        private ApplicationUserManager _userManager = new UserManagerService().UserManager;
 
         public BaseController()
         {
@@ -18,9 +27,66 @@ namespace BugTrackerApi.Controllers
 
         public BugTrackerEntities DB
         {
-            get { return db; }
+            get
+            {
+                if (db == null)
+                {
+                    db = new BugTrackerEntities();
+                    db.Configuration.ProxyCreationEnabled = false;
+                }
+
+                return db;
+            }
         }
 
+        public HttpResponseMessage InvalidModelState(ModelStateDictionary modelState)
+        {
+            List<Error> errors = new List<Error>();
+
+            foreach (var item in modelState)
+            {
+                foreach (var error in item.Value.Errors)
+                {
+                    string errorMessage = "The request is invalid";
+                    string errorReason = error.Exception?.Message ?? error.ErrorMessage;
+
+                    errors.Add(new Error()
+                    {
+                        Message = errorMessage,
+                        Reason = $"[{item.Key}] - {errorReason}",
+                        Domain = Request.RequestUri.AbsoluteUri
+                    });
+                }
+            }
+
+            return customResponseMsgService
+                .ReturnResponse(Request,
+                                HttpStatusCode.BadRequest,
+                                "The request is invalid",
+                                errors);
+        }
+
+        public HttpResponseMessage StatusOk<T>(T arg)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, arg);
+        }
+
+
+        public string CurrentAspNetUserId
+        {
+            get
+            {
+                return RequestContext.Principal.Identity.GetUserId();
+            }
+        }
+
+        public int CurrentUserId
+        {
+            get
+            {
+                return this._userManager.FindById(CurrentAspNetUserId).UserId;
+            }
+        }
 
         protected override void Dispose(bool disposing)
         {
