63e1af9d7daeceaab3d05d587664bb2e714b9abb
Darrel Abello <netzon.darrel@gmail.com>
2017-09-11 18:44:36 +0800

    Add get api for priority types and status

---
+++ BugTrackerApi/Areas/PriorityTypes/Controllers/PriorityTypeController.cs
@@ -0,0 +1,28 @@
+﻿using BugTrackerApi.Controllers;
+using ErrorCatcher.ApiFilters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace BugTrackerApi.Areas.IssuePriorityType.Controllers
+{
+    [CustomApiHandler]
+    [Authorize]
+    [RoutePrefix("PriorityType")]
+    public class PriorityTypeController : BaseController
+    {
+
+        [HttpGet]
+        [Route("")]
+        public async Task<HttpResponseMessage> GetList()
+        {
+            var issuePriorityTypes = DB.PriorityTypes.ToList();
+
+            return StatusOk(issuePriorityTypes);
+        }
+    }
+}
---
+++ BugTrackerApi/Areas/Statuses/Controllers/StatusController.cs
@@ -0,0 +1,29 @@
+﻿using BugTrackerApi.Controllers;
+using ErrorCatcher.ApiFilters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace BugTrackerApi.Areas.IssueStatuses.Controllers
+{
+    [CustomApiHandler]
+    [Authorize]
+    [RoutePrefix("Status")]
+    public class StatusController : BaseController
+    {
+
+        [HttpGet]
+        [Route("")]
+        public async Task<HttpResponseMessage> GetList()
+        {
+            var statuses = DB.IssueStatuses.ToList();
+
+            return StatusOk(statuses);
+        }
+
+    }
+}
