0879024cca94886f6154f0e825a375317a528552
Darrel Abello <darrel.abello@ilucent.com>
2017-09-14 23:16:31 -0700

    Implement invite user api

--- BugTrackerApi/Areas/Users/Controllers/UserController.cs
+++ BugTrackerApi/Areas/Users/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using BugTrackerApi.Controllers;
+using EmailSender;
 using ErrorCatcher.ApiFilters;
 using System;
 using System.Collections.Generic;
@@ -22,6 +23,20 @@ namespace BugTrackerApi.Areas.Users.Controllers
             var users = DB.GET_list_users();
 
             return StatusOk(users);
+        }
+
+        // Send an invite email
+        [Route("Invite")]
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<HttpResponseMessage> Invite()
+        {
+            var emailService = new EmailService();
+            emailService.Subject = "Test subject";
+            emailService.Body = " Test Body";
+            emailService.Send("testnidarrel@mailinator.com", "tst ni darrel");
+
+            return StatusOk();
         }
     }
 }
---
+++ EmailSender/EmailService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailSender
+{
+    public class EmailService
+    {
+        private string _fromEmail;
+        private string _fromPassword;
+        private string _fromName;
+
+        public string Subject = "";
+        public string Body = "";
+
+        public EmailService()
+        {
+            this._fromEmail = ConfigurationManager.AppSettings["serviceEmail"];
+            this._fromPassword = ConfigurationManager.AppSettings["serviceEmailPassword"];
+            this._fromName = ConfigurationManager.AppSettings["serviceEmailName"];
+        }
+
+        public EmailService(string fromEmail, string fromPassword, string fromName)
+        {
+            this._fromEmail = fromEmail;
+            this._fromPassword = fromPassword;
+            this._fromName = fromName;
+        }
+
+        public void Send(string toEmail, string toName)
+        {
+            var fromAddress = new MailAddress(this._fromEmail, this._fromName);
+            var toAddress = new MailAddress(toEmail, toName);
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(fromAddress.Address, this._fromPassword),
+                Timeout = 20000
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = Subject,
+                Body = Body
+            })
+            {
+                smtp.Send(message);
+            }
+        }
+    }
+}
